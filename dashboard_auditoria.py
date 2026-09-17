eef3335c815abcd328c4527ca16716aece341518
Conteúdos Online <158236705+CognaAcad@users.noreply.github.com>
2026-09-17 08:41:59 -0300

    Update dashboard_auditoria.py

--- dashboard_auditoria.py
+++ dashboard_auditoria.py
@@ -19,7 +19,7 @@ import plotly.express as px
 # ==========================================================
 
 st.set_page_config(
-    page_title="Dashboard de Auditoria SharePoint",
+    page_title="Dashboard Esteira de Produção - TI",
     page_icon="📊",
     layout="wide",
 )
