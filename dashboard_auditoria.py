d39d0f3762717b0c72de896fcba4f898542e77f9
Conteúdos Online <158236705+CognaAcad@users.noreply.github.com>
2026-09-21 13:47:06 -0300

    Update dashboard_auditoria.py

--- dashboard_auditoria.py
+++ dashboard_auditoria.py
@@ -19,12 +19,12 @@ import plotly.express as px
 # ==========================================================
 
 st.set_page_config(
-    page_title="Dashboard Esteira de Produção - TI",
+    page_title="Dashboard de Auditoria SharePoint",
     page_icon="📊",
     layout="wide",
 )
 
-st.title("📊 Dashboard Esteira de Produção - TI")
+st.title("📊 Dashboard de Auditoria SharePoint")
 
 st.caption(
     "Auditoria das entregas, controle de produção "
